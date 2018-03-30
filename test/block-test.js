27a030071e7efb3a0693214480b8868b4fa88aae
Jacky Tang <jackytck@gmail.com>
2018-03-31 00:04:24 +0800

    :white_check_mark: setup jest test

--- test/block-test.js
+++
@@ -1,5 +0,0 @@
-const Block = require('../src/block')
-
-const block = new Block({ timestamp: 'foo', lastHash: 'bar', hash: 'zoo', data: 'baz' })
-console.log(block.toString())
-console.log(Block.genesis().toString())
