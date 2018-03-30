81db954bc2245b23873aab598877195426b8bd7e
Jacky Tang <jackytck@gmail.com>
2018-03-30 18:04:48 +0800

    :white_check_mark: setup test

--- test/block-test.js
+++ test/block-test.js
@@ -2,3 +2,4 @@ const Block = require('../src/block')
 
 const block = new Block({ timestamp: 'foo', lastHash: 'bar', hash: 'zoo', data: 'baz' })
 console.log(block.toString())
+console.log(Block.genesis().toString())
