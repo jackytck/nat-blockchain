f04b9f1b53e831f1f3b1fc5e1cf173b9e95ea141
Jacky Tang <jackytck@gmail.com>
2018-03-31 00:03:28 +0800

    :sparkles: mine block

--- src/block.js
+++ src/block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256')
+
 class Block {
   constructor ({ timestamp, lastHash, hash, data }) {
     this.timestamp = timestamp
@@ -22,6 +24,18 @@ class Block {
       hash: 'f1r57-h45h',
       data: []
     })
+  }
+
+  static mineBlock ({ lastBlock, data }) {
+    const timestamp = Date.now()
+    const lastHash = lastBlock.hash
+    const hash = Block.hash({ timestamp, lastHash, data })
+
+    return new this({ timestamp, lastHash, hash, data })
+  }
+
+  static hash ({ timestamp, lastHash, data }) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString()
   }
 }
 
