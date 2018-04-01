13ef9d87740b709c22cb35e2836afd12bfcad18b
Jacky Tang <jackytck@gmail.com>
2018-04-01 12:08:50 +0800

    :white_check_mark: add test for isValidChain

--- test/blockchain.test.js
+++ test/blockchain.test.js
@@ -2,10 +2,11 @@ const Blockchain = require('../src/blockchain')
 const Block = require('../src/block')
 
 describe('Blockchain', () => {
-  let bc
+  let bc, bc2
 
   beforeEach(() => {
     bc = new Blockchain()
+    bc2 = new Blockchain()
   })
 
   it('start with genesis block', () => {
@@ -17,5 +18,24 @@ describe('Blockchain', () => {
     bc.addBlock({ data })
 
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data)
+  })
+
+  it('validates a valid chain', () => {
+    bc2.addBlock({ data: 'foo' })
+
+    expect(bc.isValidChain({ chain: bc2.chain })).toBe(true)
+  })
+
+  it('invalidates a chain with a corrupt genesis block', () => {
+    bc2.chain[0].data = 'Bad data'
+
+    expect(bc.isValidChain({ chain: bc2.chain })).toBe(false)
+  })
+
+  it('invalidates a corrupt chain', () => {
+    bc.addBlock({ data: 'foo' })
+    bc.chain[1].data = 'Not foo'
+
+    expect(bc.isValidChain({ chain: bc.chain })).toBe(false)
   })
 })
