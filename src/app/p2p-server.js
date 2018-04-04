7bd582e669e5691f3d7c79debb1fd849ce11885f
Jacky Tang <jackytck@gmail.com>
2018-04-04 23:52:26 +0800

    :hammer: send own chain to peer + replace own chain by peer

--- src/app/p2p-server.js
+++ src/app/p2p-server.js
@@ -35,16 +35,23 @@ class P2PServer {
     console.log('Socket connected')
 
     this.messageHandler(socket)
-
-    socket.send(JSON.stringify(this.blockchain.chain))
+    this.sendChain(socket)
   }
 
   messageHandler (socket) {
     socket.on('message', message => {
       const data = JSON.parse(message)
       console.log('data', data)
+      const chain = JSON.parse(message)
+
+      this.blockchain.replaceChain({ chain })
     })
   }
+
+  sendChain (socket) {
+    socket.send(JSON.stringify(this.blockchain.chain))
+  }
+
 }
 
 module.exports = P2PServer
