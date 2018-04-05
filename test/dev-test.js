e77d6cb9dea73532ef5fc8c63ee45dd8d710c844
Jacky Tang <jackytck@gmail.com>
2018-04-06 00:06:55 +0800

    :white_check_mark: test Block.adjustDifficulty

--- test/dev-test.js
+++ test/dev-test.js
@@ -1,1 +1,7 @@
-console.log('nat')
+const Blockchain = require('../src/blockchain')
+
+const bc = new Blockchain()
+
+for (let i = 0; i < 10; i++) {
+  console.log(bc.addBlock({ data: `foo ${i}` }).toString())
+}
