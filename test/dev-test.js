f45bef9139df57075492d0ce0aada95d58b8a77e
Jacky Tang <jackytck@gmail.com>
2018-04-07 00:15:01 +0800

    :white_check_mark: test wallet key-pair

--- test/dev-test.js
+++ test/dev-test.js
@@ -1,7 +1,11 @@
-const Blockchain = require('../src/blockchain')
+// const Blockchain = require('../src/blockchain')
+//
+// const bc = new Blockchain()
+//
+// for (let i = 0; i < 10; i++) {
+//   console.log(bc.addBlock({ data: `foo ${i}` }).toString())
+// }
 
-const bc = new Blockchain()
-
-for (let i = 0; i < 10; i++) {
-  console.log(bc.addBlock({ data: `foo ${i}` }).toString())
-}
+const Wallet = require('../src/wallet')
+const wallet = new Wallet()
+console.log(wallet.toString())
