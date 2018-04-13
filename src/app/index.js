71eba596d38c4d43de5ce74ef5ea9d9f76bfc211
Jacky Tang <jackytck@gmail.com>
2018-04-14 00:10:02 +0800

    :sparkles: add api: /transactions + /transact

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,15 @@ const Blockchain = require('../blockchain')
 const P2PServer = require('./p2p-server')
 const bodyParser = require('body-parser')
 const express = require('express')
+const Wallet = require('../wallet')
+const TransactionPool = require('../wallet/transaction-pool')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 const bc = new Blockchain()
+const wallet = new Wallet()
+const tp = new TransactionPool()
 const p2pServer = new P2PServer({ blockchain: bc })
 
 app.use(bodyParser.json())
@@ -23,6 +27,16 @@ app.post('/mine', (req, res) => {
   p2pServer.syncChains()
 
   res.redirect('/blocks')
+})
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions)
+})
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body
+  wallet.createTransaction({ recipient, amount, transactionPool: tp })
+  res.redirect('/transactions')
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}...`))
