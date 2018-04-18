c3cb9d9648b5510214eccfcb9b9746a71a604a8a
Jacky Tang <jackytck@gmail.com>
2018-04-19 00:18:06 +0800

    :sparkles: add api: /mine-transactions

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const Wallet = require('../wallet')
 const TransactionPool = require('../wallet/transaction-pool')
+const Miner = require('./miner')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
@@ -12,6 +13,7 @@ const bc = new Blockchain()
 const wallet = new Wallet()
 const tp = new TransactionPool()
 const p2pServer = new P2PServer({ blockchain: bc, transactionPool: tp })
+const miner = new Miner({ blockchain: bc, transactionPool: tp, wallet, p2pServer })
 
 app.use(bodyParser.json())
 
@@ -38,6 +40,12 @@ app.post('/transact', (req, res) => {
   const transaction = wallet.createTransaction({ recipient, amount, transactionPool: tp })
   p2pServer.broadcastTransaction({ transaction })
   res.redirect('/transactions')
+})
+
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine()
+  console.log(`New block added: ${block.toString()}`)
+  res.redirect('/blocks')
 })
 
 app.get('/public-key', (req, res) => {
